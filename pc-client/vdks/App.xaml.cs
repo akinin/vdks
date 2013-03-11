81320cde2551bb663bb13e0a66318a6821f7cbc2
Антон Миценгендлер <anton_mits@mail.ru>
2013-03-12 00:24:36 +0400

    клиент для ПК
    скелет фреймворка

---
+++ pc-client/vdks/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+//using vdks.Models;
+using vdks.ViewModels;
+using vdks.Views;
+
+namespace vdks
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+           
+            MainView view = new MainView(); // создали View
+            MainViewModel viewModel = new ViewModels.MainViewModel(); // Создали ViewModel
+            view.DataContext = viewModel; // положили ViewModel во View в качестве DataContext
+            view.Show();
+        }
+    }
+}
